cf00b9f43f6bb32c41ed0c579587c12bf9b1940e
naturalistcharlie <naturalist.charlie@gmail.com>
2014-05-10 21:59:03 -0400

    Particle Pesticides, More Plant Tweaks

--- Assets/PoopShooter.js
+++ Assets/PoopShooter.js
@@ -3,6 +3,7 @@
 	var bulletPrefab: Transform;  
 	var shootPrefab: Transform;
 	var poopspawn: Transform;
+	var pestspawn: Transform;
 	var respawnPoint: Transform;
 
 	//dying
@@ -28,24 +29,25 @@
 		Debug.Log("trigger enter");
 	    dead = true;
 	}
+	
 
 	function Update () 
 	{
 
 		if(Input.GetButtonDown("Fire2"))
 	{
-	var bullet = Instantiate(bulletPrefab, //shooting the little rabbit turds
+	var bullet = Instantiate(bulletPrefab, //shooting the little pesticides
 							poopspawn.position, 
 							Quaternion.identity);
 							//bullet.gameObject.tag = "enemyProjectile"; ///TAG TO MAKE RABBIT SPIN only here temp.
 
-	bullet.rigidbody.AddForce(transform.forward * 1000);
+	bullet.rigidbody.AddForce(transform.forward * 10);
 	}
 	
 if(Input.GetButtonDown("Fire1"))
 	{
 	var shoot = Instantiate(shootPrefab, //shooting the little rabbit turds
-							poopspawn.position,
+							pestspawn.position,
 							Quaternion.identity);
 							//bullet.gameObject.tag = "enemyProjectile"; ///TAG TO MAKE RABBIT SPIN only here temp.
 
--- Assets/Standard Assets/Scripts/General Scripts/Pesticide.js
+++ Assets/Standard Assets/Scripts/General Scripts/Pesticide.js
@@ -5,6 +5,7 @@ var projectile : Rigidbody;
   
   function OnCollisionEnter(collision : Collision)
   {
+  {
 
       if (collision.gameObject != null
         && collision.gameObject.tag == "invasive"
@@ -13,12 +14,7 @@ var projectile : Rigidbody;
         Destroy(gameObject);
         Destroy(collision.gameObject);
         }
-        if (collision.gameObject != null
-        && collision.gameObject.tag == "dirt"
-        && gameObject.tag == "pesticide")
-    {
         
-        Destroy(gameObject);
     }
       if (collision.gameObject != null
         && collision.gameObject.tag == "plant"
@@ -27,15 +23,6 @@ var projectile : Rigidbody;
         Destroy(gameObject);
         Destroy(collision.gameObject);
         }
+ 
    }
-   
-	InvokeRepeating("LaunchProjectile", TreeMature, SeedFreq);
-//	}
-
-
-function LaunchProjectile () {
-		instance = Instantiate(projectile,SeedOrigin.transform.position,Quaternion.identity);
-		instance.velocity = Random.insideUnitSphere * 0.2;
-		}
-		
-		
+	
