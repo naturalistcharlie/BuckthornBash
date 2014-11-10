2f91be10d3af15623830b1697dfaa63080f20363
naturalistcharlie <naturalist.charlie@gmail.com>
2014-11-09 19:41:45 -0500

    Minor Updates, Released 1.2!
    Released 1.2, just removed a code bug and updated readme. Need to tackle
    the issue of seed overpopulation next.

--- Assets/Standard Assets/Scripts/General Scripts/InvasiveBehavior.js
+++ Assets/Standard Assets/Scripts/General Scripts/InvasiveBehavior.js
@@ -4,6 +4,7 @@ var hirand : float = 30.0;
 var SeedOrigin : Transform;
 var dynamic : GameObject;
 var maxseed : float = 400;
+var maxbuckthorn : float = 200;
 
 function Start () {
  	var timeTillGermination : float = Random.Range(lowrand, hirand); // X to Y seconds
@@ -13,25 +14,60 @@ Invoke("GerminationCycle", timeTillGermination);
 }
 
 function GerminationCycle () {
+
+  // Because the population is stored in a static variable, you don't
+  // need to get the instance.  So we can comment out the next line.
+
+  //GetComponent(BuckthornSeedPopulation).seedpopulation;
+
+
+  // This looks good.
+  
+// testing removal of this:  if (BuckthornSeedPopulation.seedpopulation <= maxseed)
+  {
+    SeedDrop();
+
+    // Set up a new timer for the next germination.
+    var timeTillGermination : float = Random.Range(lowrand, hirand); // X to Y seconds
+
+    // This will call itself in [X, Y] seconds.
+    Invoke("GerminationCycle", timeTillGermination);
+  }
+  /* Problem: The semi-colon after "else" is an issue.  Normally we expect that
+
+     if (A) {
+       doB();
+     } else {
+       doC();
+     }
+
+     If A is true, run doB(), otherwise doC(); Which is what we want,
+     let's say.  However, that semi-colon changes it to read like
+     this:
+
+     if (A) {
+       doB();
+     } else;
+
+     {
+       doC();
+     }
+
+     If A is true, run doB(). Then run doC() regardless of what A is.
+
+     However, even if that is fixed, I don't why the code still isn't working.
+     Are you getting errors or just the wrong behavior?
+  */
+
+  //else;
+//  else
+  {
+    var timeTillGermination2 : float = Random.Range(lowrand, hirand); // X to Y seconds
+
+    Invoke("GerminationCycle", timeTillGermination);
+    }
     
-//    GetComponent(BuckthornSeedPopulation).seedpopulation;
-//    if (BuckthornSeedPopulation.seedpopulation <= maxseed) 
-       
-     	{
-        SeedDrop();
-
-        // Set up a new timer for the next germination.
-        var timeTillGermination : float = Random.Range(lowrand, hirand); // X to Y seconds
-
-        // This will call itself in [X, Y] seconds.
-        Invoke("GerminationCycle", timeTillGermination);
-        }
-//        
-//        else;
-//        {
-//        Invoke("GerminationCycle", timeTillGermination);
-//        }
-}
+    }
 
 function SeedDrop () {
 
